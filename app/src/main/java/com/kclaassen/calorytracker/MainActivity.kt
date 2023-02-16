48bdbb911b85bbb2816ec857ddaeb75efea83d77
kevin.claassen <claassenk@outlook.com>
2023-02-16 23:17:39 +0000

    OpenFoodApi setup

--- app/src/main/java/com/kclaassen/calorytracker/MainActivity.kt
+++ app/src/main/java/com/kclaassen/calorytracker/MainActivity.kt
@@ -19,6 +19,7 @@ import com.kclaassen.onboarding_presentation.age.AgeScreen
 import com.kclaassen.onboarding_presentation.gender.GenderScreen
 import com.kclaassen.onboarding_presentation.goal.GoalScreen
 import com.kclaassen.onboarding_presentation.height.HeightScreen
+import com.kclaassen.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.kclaassen.onboarding_presentation.weight.WeightScreen
 import com.kclaassen.onboarding_presentation.welcome.WelcomeScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -64,7 +65,10 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
                             ActivityScreen(
