8da8176a52b9adc52f9fe82e95949f1da6d5c3a2
kevin.claassen <claassenk@outlook.com>
2023-03-01 23:51:11 +0000

    SearchViewModel implementation

---
+++ tracker/tracker_presentation/src/main/java/com/kclaassen/tracker_presentation/search/SearchState.kt
@@ -0,0 +1,10 @@
+package com.kclaassen.tracker_presentation.search
+
+import com.kclaassen.tracker_domain.model.TrackableFood
+
+data class SearchState(
+    val query: String = "",
+    val isHintVisible: Boolean = false,
+    val isSearching: Boolean = false,
+    val trackableFood: List<TrackableFoodUiState> = emptyList()
+)
