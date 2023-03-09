ad22dba882405947efd96e194274d707743216db
kevin.claassen <claassenk@outlook.com>
2023-03-09 21:45:01 +0000

    End to End UI Test for tracking food

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionCode = ProjectConfig.versionCode
         versionName = ProjectConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.kclaassen.calorytracker.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
